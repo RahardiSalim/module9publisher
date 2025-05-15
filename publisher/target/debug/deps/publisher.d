1466e11510aad28e4de808ee4b887bcffd4547a0
RahardiSalim <rahardi.salim@ui.ac.id>
2025-05-16 00:32:05 +0700

    Initial publisher code.

---
+++ publisher/target/debug/deps/publisher.d
@@ -0,0 +1,5 @@
+C:\Users\Asus\Documents\adpro tugas ind\Tugas 9\Publisher\publisher\target\debug\deps\publisher.exe: src\main.rs
+
+C:\Users\Asus\Documents\adpro tugas ind\Tugas 9\Publisher\publisher\target\debug\deps\publisher.d: src\main.rs
+
+src\main.rs:
---
+++ publisher/target/debug/publisher.d
@@ -0,0 +1,1 @@
+C:\Users\Asus\Documents\adpro\ tugas\ ind\Tugas\ 9\Publisher\publisher\target\debug\publisher.exe: C:\Users\Asus\Documents\adpro\ tugas\ ind\Tugas\ 9\Publisher\publisher\src\main.rs
